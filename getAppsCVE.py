d0152c506c5157bc6b81a582771b83b60a755252
Ahmed Muhannad <ahmedmhj.nu@gmail.com>
2023-05-29 14:26:44 +0300

    fixed windows cve detector

--- getAppsCVE.py
+++ getAppsCVE.py
@@ -8,16 +8,20 @@ import subprocess
 from custom_prompts import custom_prompt
 import re
 
-#these values can be variant for each PC and should be changed accordingly or extended
-#these values represent the programs where the vendor is a part of the product name so it should not be removed
-whitelist = ["python", "apache netbeans", "openssl" ,"power", "postman", "gom", "opera", "mongodb", "github", "HI-TECH"]
-#regex to match edition formats
+# these values can be variant for each PC and should be changed accordingly or extended
+# these values represent the programs where the vendor is a part of the product name so it should not be removed
+whitelist = ["python", "apache netbeans", "openssl", "power",
+             "postman", "gom", "opera", "mongodb", "github", "HI-TECH"]
+# regex to match edition formats
 Edition_pattern = r"\b([a-zA-Z]+) Edition\b"
-#regex to match version numbers formats
+# regex to match version numbers formats
 version_pattern = r'\b\w*[a-zA-Z]\w*[.\d]+\w*\b|\b\w*[.\d]+\w*[a-zA-Z]\w*\b|\d+(\.\d+)+'
 
-system_cpes = ["cpe:2.3:o:microsoft:windows_10:{}:*:*:*:*:*:*:*","cpe:2.3:o:microsoft:windows:-:*:*:*:*:*:*:*","cpe:2.3:o:microsoft:windows_10:*:*:*:*:*:*:*:*"]
-version,arch = getVersion()
+system_cpes = ["cpe:2.3:o:microsoft:windows_10:{}:*:*:*:*:*:*:*",
+               "cpe:2.3:o:microsoft:windows:-:*:*:*:*:*:*:*", "cpe:2.3:o:microsoft:windows_10:*:*:*:*:*:*:*:*"]
+version, arch, release, cpe_arch = getVersion()
+
+
 def getAppsCPE():
     apps_data = json.load(open('apps.json'))
     for app in apps_data:
@@ -25,29 +29,35 @@ def getAppsCPE():
             continue
         version_parts = app.get("Version").split(".")
         version = ".".join(version_parts[:3])
-        cpe_str,score = get_best_cpe(app.get("ProgramName").replace(" ","_"), app.get("Vendor").replace(" ","_"), version)
+        cpe_str, score = get_best_cpe(app.get("ProgramName").replace(
+            " ", "_"), app.get("Vendor").replace(" ", "_"), version)
         if score < 0.6:
             app["cpe"] = "N/A"
-            print(custom_prompt('warning', f"No appropriate matches for: {app.get('ProgramName')}\n"))
+            print(custom_prompt(
+                'warning', f"No appropriate matches for: {app.get('ProgramName')}\n"))
         elif score < 0.9 and score >= 0.6:
-            print(custom_prompt('warning', f"Found possible match for: {app.get('ProgramName')}\n"))
-            choice = input(custom_prompt('warning', f"Would you like to use {cpe_str}? (y/n) Default: y\n")).lower()
+            print(custom_prompt(
+                'warning', f"Found possible match for: {app.get('ProgramName')}\n"))
+            choice = input(custom_prompt(
+                'warning', f"Would you like to use {cpe_str}? (y/n) Default: y\n")).lower()
             if choice not in ['n', 'no']:
                 app["cpe"] = cpe_str
             else:
                 app["cpe"] = "N/A"
         else:
             app["cpe"] = cpe_str
-            print(custom_prompt('information', f"Found appropriate match for: {app.get('ProgramName')}\n"))
+            print(custom_prompt('information',
+                  f"Found appropriate match for: {app.get('ProgramName')}\n"))
 
     with open("apps.json", "w") as outfile:
         json.dump(apps_data, outfile, indent=4)
 
+
 def getAppsCVE():
     # Save installed apps to temp file
     with open('temp.txt', 'w') as file:
-        subprocess.run(['powershell', '-File', 'Get-RemoteProgram.ps1'], stdout=file)
-
+        subprocess.run(
+            ['powershell', '-File', 'Get-RemoteProgram.ps1'], stdout=file)
 
     with open('temp.txt') as file:
         lines = [line.strip() for line in file if ":" in line]
@@ -60,14 +70,14 @@ def getAppsCVE():
         prop = prop.replace('\0', '').strip()
         val = val.replace('\0', '').strip()
         # Extract Edition from ProgramName field
-        if(prop == "ProgramName"):
+        if (prop == "ProgramName"):
             Edition_match = re.search(Edition_pattern, val)
             if Edition_match:
                 program["Edition"] = Edition_match.group(1)
             else:
                 program["Edition"] = "N/A"
         # remove blacklist words from program name and vendor
-        if(prop == "Vendor" or prop == "ProgramName"):
+        if (prop == "Vendor" or prop == "ProgramName"):
             val = sanitize(val.strip())
 
         # Extract Version from ProgramName field
@@ -75,10 +85,11 @@ def getAppsCVE():
             match = re.search(version_pattern, program.get("ProgramName"))
             if match:
                 tmp = match.group()
-                if((len(val) > len(tmp) and len(tmp) > 0) or len(val) == 0):
+                if ((len(val) > len(tmp) and len(tmp) > 0) or len(val) == 0):
                     val = tmp
                 # program["ProgramName"] = (program.get("ProgramName").replace(tmp, "")).strip()
-                program["ProgramName"] = re.sub('\s+', ' ', program.get("ProgramName").replace(tmp, "")).strip()
+                program["ProgramName"] = re.sub(
+                    '\s+', ' ', program.get("ProgramName").replace(tmp, "")).strip()
 
         program[prop] = val
 
@@ -86,11 +97,13 @@ def getAppsCVE():
         if prop == "SystemComponent":
             if not program.get("SystemComponent") and program.get("Vendor").lower() != "microsoft" and "Windows Driver Package" not in program.get("ProgramName"):
                 # Remove vendor name from program name unless it is part of the program name
-                if(program.get("Vendor") in program.get("ProgramName") and program.get("Vendor") != program.get("ProgramName") and program.get("Vendor").lower() not in whitelist):
-                    program["ProgramName"] = (program.get("ProgramName").replace(program.get("Vendor"), "")).strip()
+                if (program.get("Vendor") in program.get("ProgramName") and program.get("Vendor") != program.get("ProgramName") and program.get("Vendor").lower() not in whitelist):
+                    program["ProgramName"] = (program.get("ProgramName").replace(
+                        program.get("Vendor"), "")).strip()
 
                 if program.get("Version").replace(".", "") in program.get("ProgramName"):
-                    program["ProgramName"] = (program.get("ProgramName").replace(program.get("Version").replace(".", ""), "")).strip()
+                    program["ProgramName"] = (program.get("ProgramName").replace(
+                        program.get("Version").replace(".", ""), "")).strip()
 
                 programs.append(program)
                 program = {}
@@ -98,7 +111,7 @@ def getAppsCVE():
     # Include last program in cases where SystemComponent field is not included
     if not program.get("SystemComponent") and program.get("ProgramName"):
         programs.append(program)
-    
+
     for i in system_cpes:
         program["Edition"] = "N/A"
         program["ProgramName"] = ("Windows")
@@ -113,8 +126,10 @@ def getAppsCVE():
     print(custom_prompt('information', "Saved installed application to apps.json...\n"))
     os.remove('temp.txt')
 
-    print(custom_prompt('information', "Searching for suitable CPE strings for installed applications...\n"))
+    print(custom_prompt('information',
+          "Searching for suitable CPE strings for installed applications...\n"))
     getAppsCPE()
 
-    print(custom_prompt('information', "Searching for CVEs for installed applications...\n"))
+    print(custom_prompt('information',
+          "Searching for CVEs for installed applications...\n"))
     append_cve_details()
--- getCVEDetails.py
+++ getCVEDetails.py
@@ -37,7 +37,9 @@ SELECT c.CVE_ID, c.DESCRIPTION, m.BASE_SCORE, m.BASE_SEVERITY, m.VECTOR_STRING
 FROM NVD_CVE c
 INNER JOIN NVD_METRIC_CVSS m ON m.NVD_CVE_ID = c.id
 WHERE c.CVE_ID = ?;"""
-def get_cve_details(cve_id):
+
+
+def get_cve_details(cve_id, KB):
     db_path = Path('shawarma.db')
     with sqlite3.connect(str(db_path)) as conn:
         c = conn.cursor()
@@ -48,13 +50,14 @@ def get_cve_details(cve_id):
         cve_details = {
             "DESCRIPTION": cve_details_items[1],
             "CVSS_SCPRE": cve_details_items[2],
-            "AFFECTED_VERSION": 'N/A',
+            "AFFECTED_VERSION": KB,
             "SEVERITY": cve_details_items[3],
             "VECTOR_STRING": cve_details_items[4],
         }
     return cve_details
 
-def compare_versions(pkg_version,cve_version,isInclude,isLess):
+
+def compare_versions(pkg_version, cve_version, isInclude, isLess):
     if cve_version == None or cve_version == "":
         return True
     try:
@@ -75,25 +78,25 @@ def compare_versions(pkg_version,cve_version,isInclude,isLess):
         else:
             return (pkgV > cveV)
 
+
 def compare_cpe_strings(cpe1, cpe2):
     cpe1_parts = cpe1.split(":")
     cpe2_parts = cpe2.split(":")
-    
+
     if len(cpe1_parts) != len(cpe2_parts):
         return False
-    
+
     for i in range(len(cpe1_parts)):
-        if i == 5: # skip the version part
+        if i == 5:  # skip the version part
             continue
         if cpe1_parts[i] != cpe2_parts[i] and cpe2_parts[i] != "*":
             return False
-    
+
     return True
 
 
+def search_cpe_CVEs(cpe_string, version, cpe_index):
 
-def search_cpe_CVEs(cpe_string,version,cpe_index):
-    
     cve_details = {}
     db_path = Path("shawarma.db")
 
@@ -103,29 +106,35 @@ def search_cpe_CVEs(cpe_string,version,cpe_index):
         parts[5] = '*'
         with_edition_no_version = ':'.join(parts)
         parts[5] = version
-        cpe_string = ':'.join(parts) #with edition and version
+        cpe_string = ':'.join(parts)  # with edition and version
         parts[9] = '*'
         no_edition_with_version = ':'.join(parts)
         parts[5] = '*'
         no_edition_no_version = ':'.join(parts)
-        c.execute("SELECT * FROM NVD_CVE_MATCH WHERE URI = ? OR URI = ? OR URI = ? OR URI = ?", (cpe_string,with_edition_no_version, no_edition_with_version, no_edition_no_version))
+        c.execute("SELECT * FROM NVD_CVE_MATCH WHERE URI = ? OR URI = ? OR URI = ? OR URI = ?",
+                  (cpe_string, with_edition_no_version, no_edition_with_version, no_edition_no_version))
         cve_match_items = c.fetchall()
         if len(cve_match_items) == 0:
             return None
-        
-        for cve_match_item in cve_match_items: #cve_match_item == CVE_MATCH ELEMENT
+
+        for cve_match_item in cve_match_items:  # cve_match_item == CVE_MATCH ELEMENT
             Vulnerable = cve_match_item[2]
             if Vulnerable == 1:
                 cve_match_flag = True
-                for i in range(4,8):
-                    cve_match_flag = cve_match_flag and compare_versions(version,cve_match_item[i],i%2==0,i>5)
+                for i in range(4, 8):
+                    cve_match_flag = cve_match_flag and compare_versions(
+                        version, cve_match_item[i], i % 2 == 0, i > 5)
                 if cve_match_flag or (cpe_string == cve_match_item[3]):
-                    
-                    c.execute(same_parent_query,(cve_match_item[1],cve_match_item[1])) #GET ALL CHILDREN WITH THE SAME PARENT OF CVE_MATCH_ITEM
+
+                    # GET ALL CHILDREN WITH THE SAME PARENT OF CVE_MATCH_ITEM
+                    c.execute(same_parent_query,
+                              (cve_match_item[1], cve_match_item[1]))
                     cve_configurations_same_parent = c.fetchall()
-                    
-                    if len(cve_configurations_same_parent) == 0: #NO CHILDREN WITH THE SAME PARENT
-                        c.execute(cve_details_query_ConfID,(cve_match_item[1],))
+
+                    # NO CHILDREN WITH THE SAME PARENT
+                    if len(cve_configurations_same_parent) == 0:
+                        c.execute(cve_details_query_ConfID,
+                                  (cve_match_item[1],))
                         cve_details_items = c.fetchone()
                         cve_details[cve_details_items[0]] = {
                             "DESCRIPTION": cve_details_items[1],
@@ -137,26 +146,31 @@ def search_cpe_CVEs(cpe_string,version,cpe_index):
                         continue
 
                     cve_configuration_same_parent_flag = True
-                    for cve_configuration_same_parent_item in cve_configurations_same_parent: 
-                        c.execute("SELECT * FROM NVD_CVE_MATCH WHERE NVD_CVE_CONFIGURATION_ID = ?", (cve_configuration_same_parent_item[0],))
+                    for cve_configuration_same_parent_item in cve_configurations_same_parent:
+                        c.execute("SELECT * FROM NVD_CVE_MATCH WHERE NVD_CVE_CONFIGURATION_ID = ?",
+                                  (cve_configuration_same_parent_item[0],))
                         same_parent_cve_matches = c.fetchall()
                         for same_parent_cve_match in same_parent_cve_matches:
                             or_flag = False
                             parts = same_parent_cve_match[3].split(':')
-                            vendor,product = parts[3],parts[4]
+                            vendor, product = parts[3], parts[4]
                             vendor_product_part = vendor + ":" + product
                             if cpe_index.get(vendor_product_part):
-                                app_version = cpe_index.get(vendor_product_part).get("Version")
-                                app_cpe = cpe_index.get(vendor_product_part).get("cpe")
-                                if compare_cpe_strings(app_cpe,same_parent_cve_match[3]):
+                                app_version = cpe_index.get(
+                                    vendor_product_part).get("Version")
+                                app_cpe = cpe_index.get(
+                                    vendor_product_part).get("cpe")
+                                if compare_cpe_strings(app_cpe, same_parent_cve_match[3]):
                                     or_flag = True
-                                    for i in range(4,8):
-                                        or_flag = or_flag and compare_versions(app_version,same_parent_cve_match[i],i%2==0,i>5)
+                                    for i in range(4, 8):
+                                        or_flag = or_flag and compare_versions(
+                                            app_version, same_parent_cve_match[i], i % 2 == 0, i > 5)
                                     if or_flag:
                                         break
                         cve_configuration_same_parent_flag = cve_configuration_same_parent_flag and or_flag
-                    if cve_configuration_same_parent_flag: #ALL CHILDREN WITH THE SAME PARENT ARE PRESENT IN THE APPS.JSON
-                        c.execute(cve_details_query_ConfID,(cve_match_item[1],))
+                    if cve_configuration_same_parent_flag:  # ALL CHILDREN WITH THE SAME PARENT ARE PRESENT IN THE APPS.JSON
+                        c.execute(cve_details_query_ConfID,
+                                  (cve_match_item[1],))
                         cve_details_items = c.fetchone()
                         cve_details[cve_details_items[0]] = {
                             "DESCRIPTION": cve_details_items[1],
@@ -166,7 +180,6 @@ def search_cpe_CVEs(cpe_string,version,cpe_index):
                             "VECTOR_STRING": cve_details_items[4],
                         }
 
-                    
     return cve_details if cve_details else None
 
 
@@ -179,20 +192,21 @@ def append_cve_details():
     for app in applications:
         if app.get('cpe') and app.get('cpe') != "N/A":
             parts = app.get('cpe').split(':')
-            vendor,product = parts[3],parts[4]
+            vendor, product = parts[3], parts[4]
             vendor_product_part = vendor + ":" + product
             cpe_index[vendor_product_part] = app
     cve_details = {}
     for app in applications:
-        print(custom_prompt("information", f"Searching for CVEs for: {app.get('ProgramName')}"))
+        print(custom_prompt("information",
+              f"Searching for CVEs for: {app.get('ProgramName')}"))
         cpe_string = app.get("cpe")
         version = app.get("Version")
         edition = app.get("Edition")
         if not cpe_string or cpe_string == "N/A" or not version:
             continue
         app_name = app.get("ProgramName")
-        sanitized_cpe = sanitize_cpe_string(cpe_string,version,edition)
-        cve_match_item = search_cpe_CVEs(sanitized_cpe,version, cpe_index)
+        sanitized_cpe = sanitize_cpe_string(cpe_string, version, edition)
+        cve_match_item = search_cpe_CVEs(sanitized_cpe, version, cpe_index)
         if cve_match_item:
             cve_details[app_name] = cve_match_item
     with open("cve_details.json", "r") as f:
